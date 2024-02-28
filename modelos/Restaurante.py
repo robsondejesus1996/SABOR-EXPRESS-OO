c450e9bbf797c8dce11cb574ae887de9773aef8c
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-02-28 11:14:39 -0300

    Atualização para classes

---
+++ modelos/Restaurante.py
@@ -0,0 +1,13 @@
+
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = Restaurante()
+restaurante_pizza = Restaurante()
+
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(restaurantes)
