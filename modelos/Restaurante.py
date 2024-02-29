913337e6f096da5f9eac4644d4bde609cb73d8f7
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-02-29 14:59:23 -0300

    Atalização de métodos de classes

--- modelos/Restaurante.py
+++ modelos/Restaurante.py
@@ -11,9 +11,10 @@ class Restaurante:
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f"{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}")
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
                 print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
 
@@ -22,9 +23,13 @@ class Restaurante:
     @property
     def ativo(self):
         return '⌧' if self._ativo else '☐'
+    
+
+    def alternar_estado(self):
+         self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca._nome = 'Praca 2.0'
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('pizza Express', 'Italiana')
 
 
