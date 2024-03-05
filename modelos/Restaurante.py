38311fdc94e8c9ddd0c69cea564d7c4fc3d1aa58
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-03-05 11:37:57 -0300

    Exercicios Livro

--- modelos/Restaurante.py
+++ modelos/Restaurante.py
@@ -16,9 +16,11 @@ class Restaurante:
     
     @classmethod
     def listar_restaurantes(cls):
-        print(f"{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}")
+        print(f"{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}")
         for restaurante in cls.restaurantes:
-                print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+                #print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
+                print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo} ')
+
 
 
 
@@ -36,14 +38,14 @@ class Restaurante:
          avaliacao = Avaliacao(cliente, nota)
          self._avaliacao.append(avaliacao)
 
-    @property
+
     def media_avaliacoes(self):
-         if not self._avaliacao:
+        if not self._avaliacao:
             return 0
-         soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
-         quantidade_de_nota = len(self._avaliacao)
-         media = round (soma_das_notas / quantidade_de_nota, 1)
-         return media
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
          
 
 
